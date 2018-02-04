6b54f1ac70dce992dfbb4368a8504c98e8473ead
bill <bmarnt@gmail.com>
2018-02-05 01:14:26 +0200

    new methods

--- app/src/main/java/recogniton_service/Interact.java
+++ app/src/main/java/recogniton_service/Interact.java
@@ -9,8 +9,13 @@ import android.util.Log;
 
 import com.example.bill.Activities.R;
 
+import applications.Action;
 import applications.AppIntentService;
 import applications.CallTel;
+import applications.Constatns;
+import applications.Switcher;
+import recognize.Constants;
+import utils.jsonparsers.Witobj;
 
 /**
  * Created by bill on 12/12/17.
@@ -19,79 +24,158 @@ import applications.CallTel;
 public abstract class Interact extends SpeechService {
 
     private final String TAG = this.getClass().getSimpleName();
-    private String[] appResp;
-    private String tel;
+    private int RETRY_FLAG = 0;
+    private int RETRY_LIMIT = 5;
+    private String result ="";
+    private boolean HAS_APP = false;
+    private Action app;
 
+    final static public String MaestroComm = "Maestro Communication Channell";
     @Override
     public void onCreate() {
         super.onCreate();
-        IntentFilter broadcastFilter = new IntentFilter(ResponseReceiver.LOCAL_ACTION);
+        /*IntentFilter broadcastFilter = new IntentFilter(ResponseReceiver.LOCAL_ACTION);
         ResponseReceiver receiver = new ResponseReceiver();
         LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
         localBroadcastManager.registerReceiver(receiver, broadcastFilter);
-
+        */
+        app = new Action();
+        IntentFilter iff= new IntentFilter(MaestroComm);
+        LocalBroadcastManager.getInstance(this).registerReceiver(MaestroReceiver,iff);
     }
+    private void CommandReceived(Intent intent){
+        String sender1 = intent.getStringExtra("Sender");
+        Log.d(TAG, sender1);
 
+
+        //Button Press
+        if(sender1.equals("BTN")){
+            // Retry flag initialization for the NULL loop of WIT
+            RETRY_FLAG = 0;
+            //app = new Action();
+            speak(getString(R.string.helper_prompt),true);
+        }
+
+        //Speech end without user feedback
+        if(sender1.equals("TTS")){
+
+        }
+
+        //Wit Object Response
+        if(sender1.equals("WIT")){
+
+            Witobj resp = (Witobj) intent.getSerializableExtra("WitOBJ");
+
+            //IF response = null
+            //Retry to catch user command - ends after RETRY_LIMIT
+
+            if(resp.getEntities() == null){
+                if (RETRY_FLAG < RETRY_LIMIT){
+                    speak(getString(R.string.command_repeat),true);
+                }
+                else {
+                    speak(getString(R.string.retry_silent_stop),false);
+                }
+                RETRY_FLAG = RETRY_FLAG + 1;
+            }
+
+
+            //Initialization Phase
+            if (app.Stage.equals(Constatns.IN_STAGE)){
+
+                String type = resp.getEntities().getIntent().get(0).getValue();
+                Log.i(TAG,"type = "+type);
+                app = Switcher.selectActionbyType(app,type);
+            }
+
+            //Data Fill Phase
+            if (app.Stage.equals(Constatns.CH_STAGE)){
+
+                //One time no multistage comminicators to pass data from appdata
+                if(!app.MultiStageCommFromStart && resp.getEntities().getAppData() != null && resp.getEntities().getAppData().get(0).getConfidence()> 0.8){
+                    app.data.put(app.Current_Key,resp.getEntities().getAppData().get(0).getValue());
+                }
+
+                //Multi stage comm gatherer
+                if(resp.getText() != null && app.waiting_data){
+                    app.data.put(app.Current_Key,resp.getText());
+                    Log.i(TAG,"data value = "+resp.getText());
+                }
+
+                //Multi Stage comm Loop
+                for(String key:app.data.keySet()) {
+                    Log.i(TAG,"data value = "+app.data.get(key));
+                    if (app.data.get(key) == null) {
+                        speak(app.data_requests.get(key),true);
+                        app.Current_Key = key;
+                        app.waiting_data = true;
+                        break;
+                    }
+                    else{
+                        app.Stage = Constatns.TR_STAGE;
+                    }
+                }
+            }
+            if (app.Stage.equals(Constatns.TR_STAGE)){
+                app = Switcher.transforminfo(app,getApplicationContext());
+            }
+
+            if (app.Stage.equals(Constatns.VR_STAGE)){
+
+                speak(getString(R.string.make_call_question),true);
+                Log.i(TAG,"result= "+result);
+
+
+
+            }
+            if (app.Stage.equals(Constatns.RUN_STAGE)){
+                app.runIntent(getApplicationContext());
+                app.Stage = Constatns.CP_STAGE;
+            }
+
+            if (app.Stage.equals(Constatns.NF_STAGE)){
+
+                speak(app.NOT_FOUND,false);
+                Log.i(TAG,"message not found  = "+app.NOT_FOUND);
+
+            }
+            if (app.Stage.equals(Constatns.CP_STAGE)){
+                Log.i(TAG,"completed");
+                setActivated(false);
+                app.Init();
+                SendMessage("");
+            }
+
+        }
+    }
+    private BroadcastReceiver MaestroReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            CommandReceived(intent);
+        }
+    };
+    private void speak (String message,boolean recognize_after){
+        Intent msg = new Intent();
+
+        StartMessage(message);
+    }
 
     @Override
     public void OnSpeechResult(String Result) {
         super.OnSpeechResult(Result);
 
-        if (isIsinteractive()) {
-            setIsinteractive(false);
-            setActivated(false);
-            if (appResp[0].equals("contact_find") && Result.equals("ναι")) {
-                CallTel.newCall(tel, this);
-            } else if (appResp[0].equals("contact_find")) {
-                StartMessage("όπως επιθυμείτε.");
-            }
-            if (appResp[0].equals("contact_find_sms")) {
-                Sms.SendMessage(Result, this.getApplicationContext(), tel);
-            }
+        this.result = Result;
+        if(result.contains("ναι")) {
+
+            app.Stage = Constatns.RUN_STAGE;
         }
+        else if(result.contains("όχι") ) {
+            Log.d(TAG,"app stage : "+app.Stage);
 
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        super.onEndOfSpeech();
-
-    }
-
-    //create receiver for run app
-    public class ResponseReceiver extends BroadcastReceiver {
-        public static final String LOCAL_ACTION =
-                "com.example.bill.speechclient.applications.appintentservice.COMMAND_DONE";
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            appResp = intent.getExtras().getStringArray(AppIntentService.RESULT);
-
-            Log.i(TAG, "Response from command is " + appResp[0]);
-
-            //check the type of command  for selecting correct actions
-            if (appResp[0].equals("contact_find")) {
-                StartMessage(getResources().getString(R.string.make_call_question));
-                tel = appResp[1];
-                setIsinteractive(true);
-                setFirst(true);
-            } else if (appResp[0].equals("contact_find_sms")) {
-                StartMessage(getResources().getString(R.string.send_sms_question));
-                tel = appResp[1];
-                setIsinteractive(true);
-                setFirst(true);
-            } else if (appResp[0].equals(context.getResources().getString(R.string.make_call_error_message)) && (appResp[0].equals("contact_find_sms") || appResp[0].equals("contact_find"))) {
-                StartMessage(appResp[0]);
-                setIsinteractive(false);
-                setActivated(false);
-            } else {
-                StartMessage(appResp[0]);
-                setIsinteractive(false);
-                setActivated(false);
-
-            }
-
+            app.Stage = Constatns.CP_STAGE;
+            speak("όπως επιθυμείτε", false);
         }
-
     }
+
+
 }
