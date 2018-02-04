a42cbe48179201b8fb8f86d9d75481b63bb749d0
bill <bmarnt@gmail.com>
2018-02-05 01:11:34 +0200

    new methods

---
+++ app/src/main/java/applications/Action.java
@@ -0,0 +1,90 @@
+package applications;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by gvol on 17/12/2017.
+ */
+
+public class Action {
+    //Config Variables
+    private String TAG = "ActionClass";
+    public String IntentAction = null;
+    public String type = "";
+    public Uri IntentURIprefix = Uri.EMPTY;
+    public String UriQuery = "";
+    public boolean RequiresVerification = false;
+    public boolean MultiStageCommFromStart = false;
+    public boolean RequiresUri = false;
+    //Data for apps demanding content like sms
+    public HashMap<String,String> data = new HashMap<>();
+    //Request data from user with message
+    public HashMap<String,String> data_requests = new HashMap<>();
+    public boolean waiting_data = false;
+    public String Current_Key = "";
+    public String Stage = Constatns.IN_STAGE;
+
+    //Messages
+    public String VERIFY_MESSAGE = "";
+    public String ACTION_FAILED = "";
+    public String NOT_FOUND = "";
+    public String LAUNCHED = "";
+
+    //Constructor
+    public Action() {
+    }
+
+
+    public void Init(){
+        IntentAction = null;
+        type = "";
+       IntentURIprefix = Uri.EMPTY;
+        UriQuery = "";
+        RequiresVerification = false;
+        MultiStageCommFromStart = false;
+       RequiresUri = false;
+        //Data for apps demanding content like sms
+        data = new HashMap<>();
+        //Request data from user with message
+       data_requests = new HashMap<>();
+         waiting_data = false;
+        Current_Key = "";
+         Stage = Constatns.IN_STAGE;
+
+        //Messages
+       VERIFY_MESSAGE = "";
+       ACTION_FAILED = "";
+        NOT_FOUND = "";
+        LAUNCHED = "";
+
+    }
+    //Run the Intent
+    public void runIntent(Context con) {
+        Stage = Constatns.CP_STAGE;
+        Intent curIntent = CreateIntent();
+        con.startActivity(curIntent);
+    }
+
+    //Helper Classes
+    private Intent CreateIntent() {
+        Intent runInt;
+        if (RequiresUri) {
+            runInt = new Intent(IntentAction, Uri.parse(IntentURIprefix + Uri.encode(UriQuery, "UTF-8")));
+        } else {
+            runInt = new Intent(IntentAction);
+        }
+        runInt.setFlags(Constatns.FLAGS);
+        return runInt;
+    }
+
+
+
+
+}
+
---
+++ app/src/main/java/applications/Switcher.java
@@ -0,0 +1,64 @@
+package applications;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import utils.ContactUtils;
+
+/**
+ * Created by gvol on 28/1/2018.
+ */
+
+public class Switcher {
+
+    public static Action selectActionbyType(Action app, String type) {
+        if (type.equals(Constatns.CALL_APP)) {
+            app = InitActionObj(
+                    app,type,Constatns.ACTION_CALL,true,"contact",
+                    "Πείτε μου το όνομα της επαφής","tel:",
+                    false,"Η επαφή δεν βρέθηκε.","CH"
+            );
+        }
+        return app;
+    }
+
+    public static Action transforminfo (Action app, Context con){
+        if (app.type.equals("make_call")){
+
+            if (ContactUtils.ContactNumber(app.data.get("contact"),con).size() > 0) {
+                app.data.put("contact", ContactUtils.ContactNumber(app.data.get("contact"), con).get(0));
+                app.UriQuery = app.data.get("contact");
+                app.Stage = Constatns.VR_STAGE;
+            }
+            else {
+
+                app.Stage = Constatns.NF_STAGE;
+            }
+        }
+
+        return app;
+    }
+
+
+    private static Action InitActionObj(Action app,String type,String IntentAction,boolean requiresUri,
+                                        String AppName,String SoundMessage,String uri,
+                                        boolean MultiStageCommFromStart,String NOT_FOUND,String Stage
+                                        ){
+
+        app.type = type;
+        app.IntentAction = IntentAction;
+        app.RequiresUri = requiresUri;
+        app.data.put(AppName, null);
+        app.data_requests.put(AppName, SoundMessage);
+        app.Current_Key = AppName;
+        app.IntentURIprefix = Uri.parse(uri);
+        app.MultiStageCommFromStart = MultiStageCommFromStart;
+        app.NOT_FOUND = NOT_FOUND;
+        app.Stage = Stage;
+        return app;
+
+    }
+
+
+}
